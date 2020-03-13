0032dbc11292ceab98ea437857550a6d02700397
DaNaRim <rangar.danarim@gmail.com>
2020-03-13 19:33:02 +0200

    lesson 2-3 + homework 2-3

---
+++ src/lesson2/Demo.java
@@ -0,0 +1,17 @@
+package lesson2;
+
+import java.util.Date;
+
+public class Demo {
+    //java
+    //1. create class with fields +
+    //2. create object with some values +
+    //3. using it +
+
+    Order order = new Order(1001, "tool2212", 140,  new Date(), new Date());
+
+    //db
+    //1. create table with columns +
+    //2. insert some data to the table +
+    //3. using it +
+}
---
+++ src/lesson2/Order.java
@@ -0,0 +1,39 @@
+package lesson2;
+
+import java.util.Date;
+
+public class Order {
+    private long id;
+    private String productName;
+    private int price;
+    private Date dateOrdered;
+    private Date dateConfirmed;
+
+    public Order(long id, String productName, int price, Date dateOrdered, Date dateConfirmed) {
+        this.id = id;
+        this.productName = productName;
+        this.price = price;
+        this.dateOrdered = dateOrdered;
+        this.dateConfirmed = dateConfirmed;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public Date getDateOrdered() {
+        return dateOrdered;
+    }
+
+    public Date getDateConfirmed() {
+        return dateConfirmed;
+    }
+}
